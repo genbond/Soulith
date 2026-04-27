3fc12ca93c35b4e498d0fd238a5aa4eabfd69085
genbond <hanagen050125@gmail.com>
2026-04-27 17:26:40 +0900

    フェーズ管理システム実装

--- scripts/battle_root.gd
+++ scripts/battle_root.gd
@@ -2,6 +2,13 @@ extends Node2D
 
 signal enemy_turn_started
 
+enum BattlePhase {
+	PLAYER_TURN,
+	ENEMY_TURN,
+	RESOLVE,
+	REWARD
+}
+
 @export var sphere_paths: Array[NodePath] = [
 	NodePath("Sphere1"),
 	NodePath("Sphere2"),
@@ -12,8 +19,8 @@ signal enemy_turn_started
 
 var spheres: Array[RigidBody2D] = []
 var launched_count: int = 0
-var is_enemy_turn: bool = false
 var enemy_manager: Node = null
+var current_phase: BattlePhase = BattlePhase.PLAYER_TURN
 
 
 func _ready() -> void:
@@ -33,18 +40,17 @@ func _ready() -> void:
 		sphere.sphere_launched.connect(_on_sphere_launched)
 		sphere.sphere_stopped.connect(_on_sphere_stopped)
 
-	_start_player_turn()
+	_enter_phase(BattlePhase.PLAYER_TURN)
 
 
 func _start_player_turn() -> void:
-	is_enemy_turn = false
 	launched_count = 0
 	for sphere: RigidBody2D in spheres:
 		sphere.start_new_turn()
 
 
 func _on_sphere_launched(launched_sphere: RigidBody2D) -> void:
-	if is_enemy_turn:
+	if current_phase != BattlePhase.PLAYER_TURN:
 		return
 
 	for sphere: RigidBody2D in spheres:
@@ -54,13 +60,13 @@ func _on_sphere_launched(launched_sphere: RigidBody2D) -> void:
 
 
 func _on_sphere_stopped(stopped_sphere: RigidBody2D) -> void:
-	if is_enemy_turn:
+	if current_phase != BattlePhase.PLAYER_TURN:
 		return
 
 	launched_count += 1
 
 	if launched_count >= spheres.size():
-		_start_enemy_turn()
+		_enter_phase(BattlePhase.ENEMY_TURN)
 		return
 
 	for sphere: RigidBody2D in spheres:
@@ -70,12 +76,48 @@ func _on_sphere_stopped(stopped_sphere: RigidBody2D) -> void:
 
 
 func _start_enemy_turn() -> void:
-	is_enemy_turn = true
 	for sphere: RigidBody2D in spheres:
 		sphere.set_launch_enabled(false)
 
 	emit_signal("enemy_turn_started")
 	print("敵のターンだ")
 	await get_tree().create_timer(enemy_turn_duration).timeout
-	print("プレイヤーのターンだ")
-	_start_player_turn()
+	_enter_phase(BattlePhase.RESOLVE)
+
+
+func _start_resolve_phase() -> void:
+	# TODO: Apply end-of-turn effects (poison, block, etc.).
+	_enter_phase(BattlePhase.PLAYER_TURN)
+
+
+func _start_reward_phase() -> void:
+	# TODO: Implement reward selection UI flow.
+	print("報酬フェーズ（未実装）")
+
+
+func _enter_phase(next_phase: BattlePhase) -> void:
+	current_phase = next_phase
+	print("フェーズ遷移: %s" % _phase_to_text(current_phase))
+
+	match current_phase:
+		BattlePhase.PLAYER_TURN:
+			_start_player_turn()
+		BattlePhase.ENEMY_TURN:
+			_start_enemy_turn()
+		BattlePhase.RESOLVE:
+			_start_resolve_phase()
+		BattlePhase.REWARD:
+			_start_reward_phase()
+
+
+func _phase_to_text(phase: BattlePhase) -> String:
+	match phase:
+		BattlePhase.PLAYER_TURN:
+			return "PLAYER_TURN"
+		BattlePhase.ENEMY_TURN:
+			return "ENEMY_TURN"
+		BattlePhase.RESOLVE:
+			return "RESOLVE"
+		BattlePhase.REWARD:
+			return "REWARD"
+	return "UNKNOWN"
